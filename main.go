c5011b3aadf998b57b9fd1e5ddb7f686c3a6e36f
Farruco Sanjurjo <madtrick@gmail.com>
2017-02-11 11:45:50 +0100

    Print stream messages to stdout

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -9,8 +10,9 @@ import (
 	"os"
 	"syscall"
 
+	"github.com/dghubble/go-twitter/twitter"
 	"github.com/dghubble/oauth1"
-	"github.com/dghubble/oauth1/twitter"
+	twitterOauth1 "github.com/dghubble/oauth1/twitter"
 )
 
 type Configuration struct {
@@ -21,26 +23,26 @@ type Configuration struct {
 func main() {
 	_, err := os.Open("./config.json")
 
+	CONSUMER_KEY := os.Getenv("CONSUMER_KEY")
+	CONSUMER_SECRET := os.Getenv("CONSUMER_SECRET")
+
+	if CONSUMER_KEY == "" {
+		log.Fatal("Empty CONSUMER_KEY env variable")
+	}
+
+	if CONSUMER_SECRET == "" {
+		log.Fatal("Empty CONSUMER_SECRET env variable")
+	}
+
 	if err != nil {
 		if e, ok := err.(*os.PathError); ok && e.Err == syscall.ENOENT {
 			var oauthConfig oauth1.Config
-
-			CONSUMER_KEY := os.Getenv("CONSUMER_KEY")
-			CONSUMER_SECRET := os.Getenv("CONSUMER_SECRET")
-
-			if CONSUMER_KEY == "" {
-				log.Fatal("Empty CONSUMER_KEY env variable")
-			}
-
-			if CONSUMER_SECRET == "" {
-				log.Fatal("Empty CONSUMER_SECRET env variable")
-			}
 
 			oauthConfig = oauth1.Config{
 				ConsumerKey:    CONSUMER_KEY,
 				ConsumerSecret: CONSUMER_SECRET,
 				CallbackURL:    "oob",
-				Endpoint:       twitter.AuthorizeEndpoint,
+				Endpoint:       twitterOauth1.AuthorizeEndpoint,
 			}
 
 			var requestToken string
@@ -74,5 +76,26 @@ func main() {
 		} else {
 			log.Fatal(err)
 		}
+	} else {
+		data, _ := ioutil.ReadFile("config.json")
+		tokens := &Configuration{}
+		json.Unmarshal(data, tokens)
+
+		config := oauth1.NewConfig(CONSUMER_KEY, CONSUMER_SECRET)
+		token := oauth1.NewToken(tokens.Token, tokens.TokenSecret)
+		httpClient := config.Client(context.TODO(), token)
+		client := twitter.NewClient(httpClient)
+
+		params := &twitter.StreamFilterParams{
+			Track:         []string{"fuck"},
+			StallWarnings: twitter.Bool(true),
+		}
+
+		stream, _ := client.Streams.Filter(params)
+
+		for m := range stream.Messages {
+			fmt.Println(m)
+		}
+
 	}
 }
