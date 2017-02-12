f81bbb97d5c709208de10f1057a43aedd0a34fe1
Farruco Sanjurjo <madtrick@gmail.com>
2017-02-12 12:58:04 +0100

    Extract logic to fetch tokens

--- main.go
+++ main.go
@@ -29,6 +29,43 @@ func meter(count *int) {
 	}
 }
 
+func getToken(consumerKey, consumerSecret string) *Configuration {
+	var oauthConfig oauth1.Config
+
+	oauthConfig = oauth1.Config{
+		ConsumerKey:    consumerKey,
+		ConsumerSecret: consumerSecret,
+		CallbackURL:    "oob",
+		Endpoint:       twitterOauth1.AuthorizeEndpoint,
+	}
+
+	var requestToken string
+	// var requestSecret string
+	// var err error
+
+	requestToken, _, _ = oauthConfig.RequestToken()
+
+	var authorizationUrl *url.URL
+
+	authorizationUrl, _ = oauthConfig.AuthorizationURL(requestToken)
+
+	fmt.Println(authorizationUrl)
+
+	fmt.Printf("Paste your PIN here: ")
+	var verifier string
+	_, err := fmt.Scanf("%s", &verifier)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	accessToken, accessSecret, err := oauthConfig.AccessToken(requestToken, "LOL", verifier)
+	token := oauth1.NewToken(accessToken, accessSecret)
+
+	configuration := &Configuration{Token: token.Token, TokenSecret: token.TokenSecret}
+
+	return configuration
+}
+
 func main() {
 	_, err := os.Open("./config.json")
 
@@ -45,38 +82,8 @@ func main() {
 
 	if err != nil {
 		if e, ok := err.(*os.PathError); ok && e.Err == syscall.ENOENT {
-			var oauthConfig oauth1.Config
 
-			oauthConfig = oauth1.Config{
-				ConsumerKey:    CONSUMER_KEY,
-				ConsumerSecret: CONSUMER_SECRET,
-				CallbackURL:    "oob",
-				Endpoint:       twitterOauth1.AuthorizeEndpoint,
-			}
-
-			var requestToken string
-			// var requestSecret string
-			// var err error
-
-			requestToken, _, _ = oauthConfig.RequestToken()
-
-			var authorizationUrl *url.URL
-
-			authorizationUrl, _ = oauthConfig.AuthorizationURL(requestToken)
-
-			fmt.Println(authorizationUrl)
-
-			fmt.Printf("Paste your PIN here: ")
-			var verifier string
-			_, err := fmt.Scanf("%s", &verifier)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			accessToken, accessSecret, err := oauthConfig.AccessToken(requestToken, "LOL", verifier)
-			token := oauth1.NewToken(accessToken, accessSecret)
-
-			configuration := &Configuration{Token: token.Token, TokenSecret: token.TokenSecret}
+			configuration := getToken(CONSUMER_KEY, CONSUMER_SECRET)
 
 			data, _ := json.Marshal(configuration)
 			ioutil.WriteFile("config.json", data, 0600)
